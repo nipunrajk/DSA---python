fcdb4e95b11b5dc5a15b9f4725f40b31b45f3db9
Nipunraj k <nipunraj99@gmail.com>
2021-09-17 13:18:58 +0530

    GCD
    Greatest common divisor a number using recursion

---
+++ Recursion/GCD.py
@@ -0,0 +1,15 @@
+""" Greatest comman divisor of two number """
+
+def gcd(a, b):
+    assert int(a) == a and int(b) == b, 'The number must be integer only'
+    if a < 0:
+        a = -1 * a
+    if b < 0:
+        b = -1 * a
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+print(gcd(-8, 12))
+
